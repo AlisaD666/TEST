786aed2c25a515cccbd8a7038d7ac5332c8efb0f
Alisa Dhelomanova <89995351029@mail.ru>
2023-03-20 21:07:26 +0300

    финальные штрихи

--- SolutionCOD/Program.cs
+++ SolutionCOD/Program.cs
@@ -1,14 +1,14 @@
-﻿string[] arrayOne = new string[4] {"git", "20", "programmer", "tester"};
+﻿string[] arrayOne = new string[4] { "git", "20", "programmer", "tester" };
 string[] arrayTwo = new string[arrayOne.Length];
 void VerificationMethod(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
-    if(array1[i].Length <= 3)
+        if (array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
-        count++;
+            array2[count] = array1[i];
+            count++;
         }
     }
 }
